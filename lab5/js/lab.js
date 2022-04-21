02f2111c76dc081c7b43c9e3b74be447d3d6192c
Travotavo <tcarlen@live.com>
2022-04-21 00:54:09 -0700

    Lab 5 finished

---
+++ lab5/js/lab.js
@@ -0,0 +1,18 @@
+/*
+  I Travis Carlen wrote the following code the 21st of April 2022.
+  - based off the code provided by Wes Modes
+*/
+
+//Defining variables
+make = "Vans";
+model = "Old Skool";
+color = 'Black';
+year = 1997;
+
+age = 2022-1997
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
