9d94adcdb2cc29240746deed18a58c4037d64c3a
Travis A. Carlen <tcarlen@ucsc.edu>
2022-06-02 12:34:50 -0700

    JSON formatted wrong

--- testingForFoodSite/js/lab.js
+++ testingForFoodSite/js/lab.js
@@ -6,9 +6,14 @@
 var name = '';
 
 // Grabbing all importent element from the website
-var recipesArray = fetch('js/recipes.JSON').then(res => res.json());
+var recipesArray = fetch('js/recipes.JSON').json();
+var recipeDictionary = {};
 console.log(recipesArray);
-console.log("Updated Version");
-function dictionizeRecipes(){
+console.log("Updated Version 1.0");
+/*function dictionizeRecipes(){
+  for (i = 0; i < recipesArray.length; i++){
+    for (k = 0; k < recipesArray[i].ingredients.length; k++){
 
-}
+    }
+  }
+}*/
