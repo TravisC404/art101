add61f1358ff83cda526ff15d8b84242045c8a79
Travis A. Carlen <tcarlen@ucsc.edu>
2022-06-02 12:41:03 -0700

    Added dictionize

--- testingForFoodSite/js/lab.js
+++ testingForFoodSite/js/lab.js
@@ -1,19 +1,30 @@
 /*
-  I Travis Carlen wrote the following code the 10th of May 2022.
+  I Travis Carlen wrote the following code the 2nd of June 2022.
 */
 
-// Other global variables of mine
-var name = '';
-
-// Grabbing all importent element from the website
+// All used global data
 var recipesArray = fetch('js/recipes.JSON').json();
 var recipeDictionary = {};
+var ingredientInput = [];
 console.log(recipesArray);
 console.log("Updated Version 1.0");
-/*function dictionizeRecipes(){
+
+//Turns the imported recipe data into a dictionary organized by ingredients
+function dictionizeRecipes(){
   for (i = 0; i < recipesArray.length; i++){
     for (k = 0; k < recipesArray[i].ingredients.length; k++){
-
+      recipeDictionary[recipesArray[i].ingredients[k]] = recipesArray[i];
     }
   }
-}*/
+}
+
+//Gets ingredient input from site elements, stores into ingredientInput array
+function getInput(){
+
+}
+
+//
+
+
+dictionizeRecipes();
+console.log(recipesArray);
