a36c74391b06f5649a1e07384b70ed75a32f1998
Travis A. Carlen <tcarlen@ucsc.edu>
2022-06-02 15:40:51 -0700

    Testing build output

--- testingForFoodSite/js/lab.js
+++ testingForFoodSite/js/lab.js
@@ -54,6 +54,7 @@ function buildOutput(){
         }
       }
   }
+  console.log("I ran :)");
 }
 
 //Build array based
@@ -66,13 +67,13 @@ function buildSite(){
 }
 
 function processOutput(){
-  getInput();
+  //getInput();
   dictionizeRecipes();
   buildOutput();
-  sortOutput();
-  buildSite();
+  //sortOutput();
+  //buildSite();
 }
 
 
 var buttonEl = document.getElementById("dictionize");
-buttonEl.addEventListener("click", dictionizeRecipes);
+buttonEl.addEventListener("click", processOutput);
