33415eeeb474f0f1afa3168cf66359be9ae9bede
Travis A. Carlen <tcarlen@ucsc.edu>
2022-06-02 13:27:07 -0700

    Finding what works

--- testingForFoodSite/js/lab.js
+++ testingForFoodSite/js/lab.js
@@ -2,17 +2,35 @@
   I Travis Carlen wrote the following code the 2nd of June 2022.
 */
 
-// All used global data
-var recipesArray = [];
-$.ajax({
-    url: 'js/recipes.json',
-    async: false,
-    dataType: 'json',
-    success: function (response){
-      recipesArray = response;
+function loadJSON(filePath) {
+  // Load json file;
+  var json = loadTextFileAjaxSync(filePath, "application/json");
+  // Parse json
+  return JSON.parse(json);
+}
+
+function loadTextFileAjaxSync(filePath, mimeType)
+{
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open("GET",filePath,false);
+  if (mimeType != null) {
+    if (xmlhttp.overrideMimeType) {
+      xmlhttp.overrideMimeType(mimeType);
     }
   }
-);
+  xmlhttp.send();
+  if (xmlhttp.status==200 && xmlhttp.readyState == 4 )
+  {
+    return xmlhttp.responseText;
+  }
+  else {
+    // TODO Throw exception
+    return null;
+  }
+}
+
+// All used global data
+var recipesArray = loadJSON('js/recipes.json');
 recipesArray = recipesArray.json();
   //.then(response => response.json())
   //.then(data => console.log(data));
@@ -20,6 +38,8 @@ var recipeDictionary = {};
 var ingredientInput = [];
 console.log(recipesArray);
 console.log("Updated Version 0.3.3");
+
+
 
 //Turns the imported recipe data into a dictionary organized by ingredients
 function dictionizeRecipes(){
