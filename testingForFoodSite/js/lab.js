a07f67bd45f42448eb64db184f88377ebba2d6e7
Travis A. Carlen <tcarlen@ucsc.edu>
2022-06-02 13:29:13 -0700

    Finding what works

--- testingForFoodSite/js/lab.js
+++ testingForFoodSite/js/lab.js
@@ -2,38 +2,10 @@
   I Travis Carlen wrote the following code the 2nd of June 2022.
 */
 
-function loadJSON(filePath) {
-  // Load json file;
-  var json = loadTextFileAjaxSync(filePath, "application/json");
-  // Parse json
-  return JSON.parse(json);
-}
-
-function loadTextFileAjaxSync(filePath, mimeType)
-{
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.open("GET",filePath,false);
-  if (mimeType != null) {
-    if (xmlhttp.overrideMimeType) {
-      xmlhttp.overrideMimeType(mimeType);
-    }
-  }
-  xmlhttp.send();
-  if (xmlhttp.status==200 && xmlhttp.readyState == 4 )
-  {
-    return xmlhttp.responseText;
-  }
-  else {
-    // TODO Throw exception
-    return null;
-  }
-}
-
 // All used global data
-var recipesArray = loadJSON('js/recipes.json');
-recipesArray = recipesArray.json();
-  //.then(response => response.json())
-  //.then(data => console.log(data));
+var recipesArray = fetch('js/recipes.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
 var recipeDictionary = {};
 var ingredientInput = [];
 console.log(recipesArray);
